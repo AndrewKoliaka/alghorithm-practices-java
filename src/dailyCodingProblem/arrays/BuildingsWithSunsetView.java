ca99109346a0cf927ff0dd365ed638c7f145df33
AKoliaka <andrei.koliaka@gettyimages.com>
2024-08-15 21:05:23 -0400

    buildings count with sunset view (medium)

---
+++ src/dailyCodingProblem/arrays/BuildingsWithSunsetView.java
@@ -0,0 +1,33 @@
+package dailyCodingProblem.arrays;
+
+public class BuildingsWithSunsetView {
+    public static void main(String[] args) {
+        int[] arr1 = { 3, 7, 8, 3, 6, 1 };
+        int[] arr2 = { 3, 7, 8, 3, 6 };
+        int[] arr3 = { 1 };
+        int[] arr4 = {};
+
+        System.out.println(getBuildingsCount(arr1));
+        System.out.println(getBuildingsCount(arr2));
+        System.out.println(getBuildingsCount(arr3));
+        System.out.println(getBuildingsCount(arr4));
+    }
+
+    private static int getBuildingsCount(int[] arr) {
+        if (arr.length == 0) {
+            return 0;
+        }
+
+        int count = 1;
+        int max = arr[0];
+
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] > max) {
+                max = arr[i];
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
